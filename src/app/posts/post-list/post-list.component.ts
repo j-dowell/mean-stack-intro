a7f88300987e0905022799a6e18a6b054774a76b
James Dowell <james.s.dowell@gmail.com>
2018-09-16 06:56:30 +0000

    imported Post model in other post components

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { Post } form '../post.model'
 
 @Component({
   selector: 'app-post-list',
@@ -20,5 +21,5 @@ export class PostListComponent {
   //     content: 'This is the third posts content'
   //   },
   // ];
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 }
