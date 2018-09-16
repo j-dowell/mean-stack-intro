0ff69f3c5e44193c2adf5d717bb106f43c33b688
James Dowell <james.s.dowell@gmail.com>
2018-09-16 05:09:15 +0000

    added ngmodel

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'No content';
 
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
 }
