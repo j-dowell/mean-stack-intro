32189c4501f7c55a0d831412c185e94d948d85ec
James Dowell <james.s.dowell@gmail.com>
2018-09-17 02:45:54 +0000

    imported angular http module

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hi from express");
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "id1",
+      title: "First server side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "id2",
+      title: "Second server side post",
+      content: "This is coming from the server"
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched succesfully",
+    posts: posts
+  });
 });
 
 module.exports = app;
